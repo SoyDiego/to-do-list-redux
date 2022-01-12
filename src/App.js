7254822c1bd7df1ed3ff6fb8b721eec862cd7251
soydiego <diegofranchina@gmail.com>
2022-01-12 13:29:13 +0100

    Init project, deleted files, started structure and installed tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="tw-h-screen tw-grid tw-place-content-center tw-align-content-center tw-bg-green-900 tw-text-center">
+			<h1 className="tw-text-white tw-font-extrabold">To-Do list</h1>
+			<h2 className="tw-text-green-600 tw-font-bold">
+				using ReactJS, Redux and TailwindCSS
+			</h2>
+
+			<div className="tw-w-full tw-flex tw-py-10 tw-px-20">
+				<div className="tw-w-4/12 tw-flex tw-flex-wrap">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore possimus cumque minus id ratione dolor quaerat eveniet repellendus pariatur ad rerum debitis odio, quas provident. Velit ad exercitationem, commodi voluptatibus reprehenderit quos repudiandae sit aspernatur autem consequuntur at recusandae nisi. Ea deserunt aspernatur esse facere eaque quae odio, quos numquam.</div>
+				<div className="tw-w-8/12 tw-flex tw-flex-wrap">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum sit vitae blanditiis perferendis, molestiae aperiam et nemo consequuntur odio veritatis! Obcaecati rerum incidunt reprehenderit inventore mollitia saepe cumque fugiat, eos aspernatur, nesciunt vitae quod architecto repellendus animi id? Quis libero, inventore reprehenderit suscipit omnis quod veritatis ea temporibus soluta excepturi deleniti dolore facere pariatur provident natus laudantium blanditiis? Debitis illum quis labore dignissimos quas earum, nobis consectetur molestiae quaerat iusto officiis delectus iure quos pariatur facilis. Optio, nulla asperiores eaque quo sit praesentium? Cumque eum fuga nostrum consectetur, corporis perferendis eos ipsam repudiandae. Error ex iure placeat vitae magni atque.</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
